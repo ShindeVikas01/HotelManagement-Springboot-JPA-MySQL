dc017a09d122f5cd3efef7436170b35a5b8fe8a3
ShindeVikas01 <shinde.vikas01@gmail.com>
2023-07-11 12:59:26 +0530

    [ADDED]:[Vikas]HotelManagement Setup

---
+++ src/test/java/com/infinity/hotelmanagment/HotelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinity.hotelmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
